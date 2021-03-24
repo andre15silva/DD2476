9c45958c56d202337cc21ddab0f2f7e10e878b7d
André Silva <andre15andre@hotmail.com>
2021-03-24 11:55:04 +0000

    Init maven project. Init spoon model.
    Signed-off-by: André Silva <andre15andre@hotmail.com>

--- indexer/src/main/java/se/kth/dd2476/RepoDecoder.java
+++ indexer/src/main/java/se/kth/dd2476/RepoDecoder.java
@@ -1,26 +1,35 @@
+package se.kth.dd2476;
+
+import spoon.Launcher;
+import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtMethod;
+
+import javax.security.auth.callback.LanguageCallback;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.*;
-import java.util.function.Consumer;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * An iterable class to decode github files.
  * It reads from the given scanner the github api URLs
  * and provides an iterator for the code that will be extracted.
  */
-public class RepoDecoder implements Iterable<String>{
+public class RepoDecoder implements Iterable<String> {
 
     Scanner scanner;
     boolean scannerIsEmpty = false;
     String name;
     ArrayList<String> URLs = new ArrayList<>();
 
-    public RepoDecoder(Scanner scanner){
+    public RepoDecoder(Scanner scanner) {
         this.scanner = scanner;
-        assert(scanner.hasNext());
+        assert (scanner.hasNext());
         name = scanner.next();
     }
 
@@ -31,9 +40,9 @@ public class RepoDecoder implements Iterable<String>{
 
             @Override
             public boolean hasNext() {
-                if(index < URLs.size())
+                if (index < URLs.size())
                     return true;
-                if(!scannerIsEmpty){
+                if (!scannerIsEmpty) {
                     scannerIsEmpty = !scanner.hasNext();
                     return !scannerIsEmpty;
                 }
@@ -42,7 +51,7 @@ public class RepoDecoder implements Iterable<String>{
 
             @Override
             public String next() {
-                if(index < URLs.size())
+                if (index < URLs.size())
                     return getDecodedString(URLs.get(index++));
                 index++;
                 String fileURL = scanner.next();
@@ -54,6 +63,7 @@ public class RepoDecoder implements Iterable<String>{
 
     /**
      * Given a github api ulr, decodes it and returns the relative code
+     *
      * @param fileURL a url of a file of the type api.github.com/<user>/<repo>/blob/sha
      * @return A string with the code
      */
@@ -71,15 +81,15 @@ public class RepoDecoder implements Iterable<String>{
             }
             in.close();
             var content = response.toString();
-            if(!content.contains("content")){
+            if (!content.contains("content")) {
                 System.err.println("Retrieved non-standard json:\n" + content);
                 return null;
             }
             content = content.split("content\":\"")[1].split("\"")[0];
             var stringBuilder = new StringBuilder();
             var decoder = Base64.getMimeDecoder();
-            for(var line: content.split("\\\\n")){
-                stringBuilder.append(new String(decoder.decode(line))).append("\n");
+            for (var line : content.split("\\\\n")) {
+                stringBuilder.append(new String(decoder.decode(line)));
             }
             return stringBuilder.toString();
         } catch (IOException e) {
@@ -90,20 +100,26 @@ public class RepoDecoder implements Iterable<String>{
 
     /**
      * Example of usage
+     *
      * @param args
      */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         var repoDecoder = new RepoDecoder(input);
-        for(String code: repoDecoder){
+        for (String code : repoDecoder) {
             spoonDoesSomething(code);
         }
 
     }
 
     private static void spoonDoesSomething(String code) {
-        System.out.println(code);
+        CtClass klass = Launcher.parseClass(code);
+
+        // indexClass(klass);
+
+        for (Object method : klass.getAllMethods()) {
+            // indexMethod((CtMethod) method);
+            System.out.println((CtMethod) method);
+        }
     }
-
-
 }
