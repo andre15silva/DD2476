da6ad61d28261f69dad711f6f34b67d83928cb09
Alan Ihre <alan.ihre@alanihre.se>
2021-04-25 11:37:45 +0200

    Move the search form into a separate component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,13 @@
 import './App.css';
 import AppBar from "./AppBar";
-import {Button, makeStyles, TextField} from '@material-ui/core/index';
-import Paper from '@material-ui/core/Paper';
+import {makeStyles} from '@material-ui/core/index';
 import Typography from '@material-ui/core/Typography';
 import Page from './Page';
 import Method from "./Method";
 import React  from 'react';
 import fetch from 'cross-fetch';
 import SearchResults from "./SearchResults";
-import ButtonGroup from '@material-ui/core/ButtonGroup';
-
-const useStyles = makeStyles({
-    searchForm: {
-        marginBottom: 20,
-    },
-    searchField: {
-        width: "90%",
-    },
-    simpleSearchField: {
-        marginLeft: 8,
-        marginRight: 8,
-        marginBottom: 8,
-    }
-});
+import SearchForm from "./SearchForm";
 
 function responseIsJson(response) {
     const contentType = response.headers.get('content-type');
@@ -75,10 +60,6 @@ function elasticSearchRequest(resource, method = 'GET', body = null) {
 }
 
 function App() {
-    const classes = useStyles();
-
-    const [advancedSearch, setAdvancedSearch] = React.useState(false);
-
     const [data, setData] = React.useState({
         methods: [],
         expanded: [],
@@ -86,79 +67,16 @@ function App() {
         error: false,
     });
 
-
-    const [nameInputValue, setNameInputValue] = React.useState("");
-    const [returnTypeInputValue, setReturnTypeInputValue] = React.useState("");
-    const [repositoryInputValue, setRepositoryInputValue] = React.useState("");
-    const [fileInputValue, setFileInputValue] = React.useState("");
-    const [visibilityInputValue, setVisibilityInputValue] = React.useState("");
-    const [javaDocInputValue, setJavaDocInputValue] = React.useState("");
-    const [modifiersInputValue, setModifiersInputValue] = React.useState("");
-    const [thrownInputValue, setThrownInputValue] = React.useState("");
-    const [annotationsInputValue, setAnnotationsInputValue] = React.useState("");
-    const [classNameInputValue, setClassNameInputValue] = React.useState("");
-    const [argumentNameInputValue, setArgumentNameInputValue] = React.useState("");
-    const [argumentTypeInputValue, setArgumentTypeInputValue] = React.useState("");
-
-    const [searchInputValue, setSearchInputValue] = React.useState('');
-
     const [hasSearched, setHasSearched] = React.useState(false);
 
-    function rowClick(e, index) {
+    const rowClick = (e, index) => {
         e.stopPropagation();
         const newData = {...data};
         newData.expanded[index] = !data.expanded[index];
         setData(newData);
     }
 
-    function onFormSubmit(e) {
-        e.preventDefault();
-
-        let query;
-        if (advancedSearch) {
-            query = searchInputValue;
-        } else {
-            let queryParts = [];
-            if (nameInputValue.length > 0) {
-                queryParts.push("name:(" + nameInputValue + ")");
-            }
-            if (returnTypeInputValue.length > 0) {
-                queryParts.push("returnType:(" + returnTypeInputValue + ")");
-            }
-            if (repositoryInputValue.length > 0) {
-                queryParts.push("repository:(" + repositoryInputValue + ")");
-            }
-            if (fileInputValue.length > 0) {
-                queryParts.push("file:(" + fileInputValue + ")");
-            }
-            if (visibilityInputValue.length > 0) {
-                queryParts.push("visibility:(" + visibilityInputValue + ")");
-            }
-            if (javaDocInputValue.length > 0) {
-                queryParts.push("javaDoc:(" + javaDocInputValue + ")");
-            }
-            if (modifiersInputValue.length > 0) {
-                queryParts.push("modifiers:(" + modifiersInputValue + ")");
-            }
-            if (thrownInputValue.length > 0) {
-                queryParts.push("thrown:(" + thrownInputValue + ")");
-            }
-            if (annotationsInputValue.length > 0) {
-                queryParts.push("annotations:(" + annotationsInputValue + ")");
-            }
-            if (classNameInputValue.length > 0) {
-                queryParts.push("className:(" + classNameInputValue + ")");
-            }
-            if (argumentNameInputValue.length > 0) {
-                queryParts.push("arguments.name:(" + argumentNameInputValue + ")");
-            }
-            if (argumentTypeInputValue.length > 0) {
-                queryParts.push("arguments.type:(" + argumentTypeInputValue + ")");
-            }
-            query = queryParts.join(' AND ');
-            setSearchInputValue(query);
-        }
-
+    const searchFunc = query => {
         elasticSearchRequest("code/method/_search", "POST", {
             "query": {
                 "query_string": {
@@ -200,126 +118,7 @@ function App() {
     <div className="App">
       <AppBar />
       <Page>
-          <Paper className={classes.searchForm}>
-              <form noValidate autoComplete="off" onSubmit={(e) => onFormSubmit(e)}>
-                  <ButtonGroup color="primary" aria-label="outlined primary button group">
-                      <Button onClick={() => setAdvancedSearch(!advancedSearch)} variant={advancedSearch ? "outlined" : "contained"}>Simple</Button>
-                      <Button onClick={() => setAdvancedSearch(!advancedSearch)} variant={advancedSearch ? "contained" : "outlined"}>Advanced</Button>
-                  </ButtonGroup>
-                  <br/> <br/>
-                  {advancedSearch ?
-                      <TextField
-                          value={searchInputValue}
-                          onInput={e => setSearchInputValue(e.target.value)}
-                          className={classes.searchField}
-                          id="outlined-basic"
-                          label="Search"
-                          variant="outlined" />
-                      :
-                      <React.Fragment>
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={nameInputValue}
-                              onInput={e => setNameInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Method name"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={returnTypeInputValue}
-                              onInput={e => setReturnTypeInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Return type"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={repositoryInputValue}
-                              onInput={e => setRepositoryInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Repository"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={fileInputValue}
-                              onInput={e => setFileInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="File name"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={visibilityInputValue}
-                              onInput={e => setVisibilityInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Visibility"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={javaDocInputValue}
-                              onInput={e => setJavaDocInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Javadoc"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={modifiersInputValue}
-                              onInput={e => setModifiersInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Modifiers"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={thrownInputValue}
-                              onInput={e => setThrownInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Throws"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={annotationsInputValue}
-                              onInput={e => setAnnotationsInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Annotations"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={classNameInputValue}
-                              onInput={e => setClassNameInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Class name"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={argumentNameInputValue}
-                              onInput={e => setArgumentNameInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Argument name"
-                              variant="outlined"
-                              size={"small"} />
-                          <TextField
-                              className={classes.simpleSearchField}
-                              value={argumentTypeInputValue}
-                              onInput={e => setArgumentTypeInputValue(e.target.value)}
-                              id="outlined-basic"
-                              label="Argument type"
-                              variant="outlined"
-                              size={"small"} />
-                      </React.Fragment>
-                     }
-                     <br/><br/>
-                    <Button color={"primary"}  variant={"contained"} type={"submit"}>Search</Button>
-                    <br/><br/>
-              </form>
-          </Paper>
+          <SearchForm searchFunc={searchFunc} />
 
           {data.error &&
             <Typography color={"error"}>An error occurred</Typography>
